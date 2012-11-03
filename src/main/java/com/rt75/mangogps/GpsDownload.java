4ede49325f1bd411ce6607ee683412bb932ff52c
bormotun <bormotun@bormotun-ubu.(none)>
2012-11-04 00:07:51 +0400

    логгер подключил

--- src/main/java/com/rt75/mangogps/GpsDownload.java
+++ src/main/java/com/rt75/mangogps/GpsDownload.java
@@ -3,17 +3,19 @@ package com.rt75.mangogps;
 import org.apache.commons.cli.*;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 
 /**
 
  */
 public class GpsDownload {
-
+    private static Logger logger= LoggerFactory.getLogger(GpsDownload.class);
 
     public static void main(String... args) throws IOException, InterruptedException, ParseException {
-
 
         Options options = new Options();
         options.addOption(OptionBuilder.withLongOpt("days")
@@ -23,7 +25,11 @@ public class GpsDownload {
                 .addOption(OptionBuilder.withLongOpt("gpsFileName")
                         .hasArgs()
                         .withDescription("gpsFileName (port)")
-                        .create("p"));
+                        .create("p"))
+                .addOption(OptionBuilder.withLongOpt("logLevel")
+                        .hasArgs()
+                        .withDescription("log level DEBUG INFO ERROR")
+                        .create("l"));
 
 
         CommandLineParser parser = new PosixParser();
@@ -31,8 +37,18 @@ public class GpsDownload {
 
         String gpsFileName = cmdLine.getOptionValue("gpsFileName", "/dev/ttyUSB0");
 
+
+        //TODO брать из параметров коммандной строки
+        if(logger instanceof ch.qos.logback.classic.Logger){
+            ((ch.qos.logback.classic.Logger) logger).setLevel(ch.qos.logback.classic.Level.valueOf(
+             cmdLine.getOptionValue("logLevel", "ERROR")
+            ));
+        }
+
+
         if(!new File(gpsFileName).exists()){
-            return;
+            logger.error("Can not open gps-com file: {}",gpsFileName);
+          //  return;
         }
 
         int minusDays = Integer.parseInt(cmdLine.getOptionValue("days", "10"));
@@ -44,10 +60,10 @@ public class GpsDownload {
             String fileName = DateTimeFormat.forPattern("yyMMdd").print(
                     new DateTime().minusDays(--minusDays)) + ".TXT";
 
-            System.out.println("try process file "+fileName);
+            logger.info("try process file {}:",fileName);
 
             if (!new File(fileName).exists()) {
-                System.out.println("process file "+fileName);
+                logger.info("process file {}:",fileName);
                 mangoGpsController.sendDownloadCommand(fileName, gpsFileName);
                 mangoGpsController.downloadFile(fileName, gpsFileName);
             }
