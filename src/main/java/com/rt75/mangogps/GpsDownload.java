e68d76b7c721ae90a20fd01fba2cb4c8069e2d1c
Vladimir Kuznetsov <vkuz@1c.ru>
2016-02-05 17:55:39 +0300

    experimental w java 8

--- src/main/java/com/rt75/mangogps/GpsDownload.java
+++ src/main/java/com/rt75/mangogps/GpsDownload.java
@@ -1,12 +1,27 @@
 package com.rt75.mangogps;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.FileList;
 import jssc.SerialPortException;
 import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -66,12 +81,84 @@ public class GpsDownload {
     }
 
 
+
+
+
+    /** Authorizes the installed application to access user's protected data. */
+    private static Credential authorize() throws Exception {
+      // load client secrets
+      GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(),
+          new InputStreamReader(new FileInputStream("/home/bormotun/Загрузки/client_secret.json")));
+      // set up authorization code flow
+      GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+              GoogleNetHttpTransport.newTrustedTransport(),
+              JacksonFactory.getDefaultInstance(), clientSecrets,
+          Collections.singleton(DriveScopes.DRIVE_FILE)).
+              setDataStoreFactory(new FileDataStoreFactory(new File("/home/bormotun/ds")))
+         .build();
+      // authorize
+        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+
+       Drive drive = new Drive.Builder(GoogleNetHttpTransport.newTrustedTransport()
+               , JacksonFactory.getDefaultInstance(), credential).setApplicationName(
+            "test").build();
+
+
+        com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+        fileMetadata.setName("1.txt");
+
+        FileContent mediaContent = new FileContent("image/jpeg", new File("/home/bormotun/filename1.txt"));
+
+        Drive.Files.Create insert = drive.files().create(fileMetadata, mediaContent);
+        MediaHttpUploader uploader = insert.getMediaHttpUploader();
+        uploader.setDirectUploadEnabled(true);
+      //  uploader.setProgressListener(new FileUploadProgressListener());
+        insert.execute();
+
+
+
+        FileList result = drive.files().list()
+             .execute();
+        List<com.google.api.services.drive.model.File> files = result.getFiles();
+        if (files == null || files.size() == 0) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (com.google.api.services.drive.model.File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
+
+
+//        File f=new java.io.File("/home/bormotun/", "recPrinter.txt");
+//
+//        OutputStream out = new FileOutputStream(f);
+//
+//        Drive.Files.Get request = drive.files().get("recPrinter.txt");
+//        request.getMediaHttpDownloader().setDirectDownloadEnabled(true);
+//
+//        request.executeMediaAndDownloadTo(out);
+
+
+
+       return credential;
+   }
+
+
     public static void main(String... args) throws InterruptedException, MangoException, SerialPortException, ParseException, IOException {
+
+
+
+
         try {
+            authorize();
+
+            if(true){
+                return;
+            }
 
 
-
-            String aaaaa="sdfvsdfv";
+            String aaaaa="12345678";
             Optional<String> optional = Optional.ofNullable(aaaaa);
 
 //            Function<String,Optional<String>> f=(s)->{
@@ -81,9 +168,9 @@ public class GpsDownload {
             Function<String,Optional<String>> f=(s)->Optional.of(s.substring(2));
 
             //flatMap не оборачивает в Optional map - оборачивает
-            System.out.println(optional.map(String::length).map(i -> i+1).get());
+            System.out.println(optional.map(String::length).map(i -> i+1).orElse(-1));
             System.out.println(optional.
-                    flatMap(s -> {return Optional.of(s.length());}).map(i -> i+1).get());
+                    flatMap(s -> {return Optional.of(s.length());}).map(i -> i+1).orElse(-1));
 
 
 
